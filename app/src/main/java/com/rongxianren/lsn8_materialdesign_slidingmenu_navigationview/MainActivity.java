b2c1392c56ef8ce9c3072a601d56495a768abdcf
rongxianren <rongxianren@163.com>
2016-11-25 14:45:42 +0800

    Material_Design_Navigation

---
+++ app/src/main/java/com/rongxianren/lsn8_materialdesign_slidingmenu_navigationview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rongxianren.lsn8_materialdesign_slidingmenu_navigationview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
